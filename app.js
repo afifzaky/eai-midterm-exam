230d4027d9a2207fdc7aaccf0d7dd198ddfeb1ff
afifzaky <afifzakym@gmail.com>
2023-04-25 04:34:47 +0700

    add middleware: Handling Error

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const bootcampRouter = require('./routes/bootcamp-router');
+const errorHandler = require('./middleware/error');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -8,8 +9,12 @@ const PORT = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// set routing
+// set routings
 app.use('/api/bootcamp', bootcampRouter);
+
+// set error middleware
+app.use(errorHandler);
 
 // buat server nya
 app.listen(PORT, () => console.log(`Server running at port: ${PORT}`));
+
